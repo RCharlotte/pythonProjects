e4b1b99f522b8f6c06bee9fa42ab21c9771371d3
RCharlote <ruthcharlotte02@gmail.com>
2022-06-04 10:32:40 +0300

    first commit

---
+++ game1.py
@@ -0,0 +1,9 @@
+
+noun= input("Noun: ")
+adjective= input("Adjective: ")
+famous_person=input("famous_person: ")
+
+
+madlib= f"The best part about {noun} is that you get to do anything you want, provided that it ir a {adjective} thing to do. You can even see {famous_person} while on some of the rides they have there"
+
+print(madlib)
